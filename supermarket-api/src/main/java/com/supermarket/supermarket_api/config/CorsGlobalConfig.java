771225a6123d3f260bea051ad21e3427bcd42159
jpdjcp <jpdjcp@gmail.com>
2026-01-02 03:16:32 -0300

    Deleted MvcConfig; Add CorsGlobalConfig

---
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.supermarket.supermarket_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://192.168.137.59"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
