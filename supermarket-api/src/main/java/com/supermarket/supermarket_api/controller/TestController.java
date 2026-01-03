b2ef5213efb4144850088947c98ffc9b5ac78d3f
jpdjcp <jpdjcp@gmail.com>
2026-01-02 21:28:48 -0300

    TestController class, add method options()

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/TestController.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.supermarket.supermarket_api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class TestController {
     @PostMapping
     public String test() {
         return "OK";
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        return ResponseEntity.ok().build();
     }
 }
