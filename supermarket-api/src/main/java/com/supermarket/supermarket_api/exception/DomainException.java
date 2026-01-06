93a536ec7125c6a32a631112f294cd8108f635b6
jpdjcp <jpdjcp@gmail.com>
2026-01-06 20:44:33 -0300

    Refactor DomainException class. Convert it in abstract class.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/DomainException.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/DomainException.java
@@ -1,6 +1,6 @@
 package com.supermarket.supermarket_api.exception;
 
-public class DomainException extends RuntimeException {
+public abstract class DomainException extends RuntimeException {
 
     protected DomainException(String message) {
         super(message);
