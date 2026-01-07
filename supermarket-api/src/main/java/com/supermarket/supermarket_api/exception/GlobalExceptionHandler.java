507cf9cb3ddce0217351bc62d38abca871afd11b
jpdjcp <jpdjcp@gmail.com>
2026-01-06 21:06:08 -0300

    Renamed GlobalHandlerException to GlobalExceptionHandler.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/GlobalExceptionHandler.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.supermarket.supermarket_api.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalHandlerException {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler({
             SaleNotFoundException.class,
