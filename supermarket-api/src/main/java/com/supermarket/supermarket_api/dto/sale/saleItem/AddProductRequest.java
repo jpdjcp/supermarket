b49f0c176995cc0e5a62847512de74f63b92c27f
jpdjcp <jpdjcp@gmail.com>
2026-01-17 00:53:26 -0300

    Refactor: removed field SaleId in AddProductRequest DTO.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/dto/sale/saleItem/AddProductRequest.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/dto/sale/saleItem/AddProductRequest.java
@@ -6,10 +6,6 @@ import jakarta.validation.constraints.Positive;
 
 public record AddProductRequest(
 
-        @NotNull(message = "Sale ID is required")
-        @Positive(message = "Sale ID must positive")
-        Long saleId,
-
         @NotNull(message = "Product ID is required")
         @Positive(message = "Product ID must be positive")
         Long productId,
