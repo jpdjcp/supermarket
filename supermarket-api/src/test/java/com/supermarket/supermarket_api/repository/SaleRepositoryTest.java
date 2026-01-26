92e2e922509fb8eed82968026825af9fa8692414
jpdjcp <jpdjcp@gmail.com>
2026-01-26 02:15:09 -0300

    Add: application-test.yml file.

---
+++ supermarket-api/src/test/java/com/supermarket/supermarket_api/repository/SaleRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.supermarket.supermarket_api.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class SaleRepositoryTest {
+}
