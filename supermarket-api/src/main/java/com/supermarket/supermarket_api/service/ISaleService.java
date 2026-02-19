8b8be4d96b4127c8a281d1906c5f9ead0d6eee05
jpdjcp <jpdjcp@gmail.com>
2026-02-19 19:32:54 -0300

    Added: findByCreatedAtBetween() to Sale's Repository.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/service/ISaleService.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/service/ISaleService.java
@@ -4,13 +4,16 @@ import com.supermarket.supermarket_api.dto.sale.SaleResponse;
 import com.supermarket.supermarket_api.dto.sale.saleItem.AddProductRequest;
 import com.supermarket.supermarket_api.dto.sale.saleItem.AddProductResponse;
 import com.supermarket.supermarket_api.dto.sale.saleItem.SaleItemResponse;
+import com.supermarket.supermarket_api.model.Sale;
 import com.supermarket.supermarket_api.model.SaleItem;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface ISaleService {
     SaleResponse createSale(Long branchId);
     SaleResponse findById(Long id);
+    List<Sale> findByCreatedAtBetween(Instant from, Instant to);
     AddProductResponse addProduct(Long id, AddProductRequest request);
     List<SaleItemResponse> getItems(Long saleId);
     SaleItem getItem(Long id, Long productId);
