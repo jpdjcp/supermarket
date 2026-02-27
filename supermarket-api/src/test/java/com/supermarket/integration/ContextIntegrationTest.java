7dc33ab98509c70ae44783c735b55fcfe2d2a1bf
jpdjcp <jpdjcp@gmail.com>
2026-02-27 00:52:13 -0300

    Config: mysql8-testcontainer.

---
+++ supermarket-api/src/test/java/com/supermarket/integration/ContextIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.supermarket.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class ContextIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoadsWithRealMySQL() {
+
+    }
+}
