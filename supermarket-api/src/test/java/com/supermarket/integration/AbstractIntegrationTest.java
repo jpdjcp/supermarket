1b367ce3db6281f25ad615759c0ca19e343398ec
Marcelo Palacios - mpcodenweb <jpdjcp@gmail.com>
2026-03-01 01:08:50 -0300

    Testcontainer setup (#44)
    * Config: mysql8-testcontainer.
    
    * Fix: add main SpringBoot class path to AbstractIntegrationTest SpringBootTest annotation.
    
    * Fix: pom.xml, testcontainers-bom version changed to 1.20.4
    
    * Fix: add docker-java-core dependency to pom.xml.
    
    * Fix: add testcontainers dependencies to pom.xml
    
    * Fix: delete duplicated dependency.
    
    * Add: DockerPingTest
    
    * Add: DockerPingManualTest
    
    * Moved DockerPingMaulaTest
    
    * Fix: pom.xml.
    
    * Fix: pom.xml incompatibilities.
    
    * Add: DockerDebuggerTest
    
    * Fix: pom.xml for DockerDebuggerTest
    
    * Fix: DockerDebugTest naming.
    
    * Add: DockerRuntimeDebugTest
    
    * Add: docker-java 3.5 to pom.xml
    
    * Add: javax.ws.rs-api to pom.xml
    
    * Delete: debug tests
    
    * Deleted: DockerPingManualTest used for debug.

---
+++ supermarket-api/src/test/java/com/supermarket/integration/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.supermarket.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(classes = com.supermarket.supermarket_api.SupermarketApiApplication.class)
+public abstract class AbstractIntegrationTest {
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("test_db")
+            .withUsername("test")
+            .withPassword("test");
+
+    @BeforeAll
+    static void startContainer() {
+        mysql.start();
+    }
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+    }
+}
