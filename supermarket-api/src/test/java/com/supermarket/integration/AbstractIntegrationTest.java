ffabbdbb272773724bb81d97720b289ba0452b7f
jpdjcp <jpdjcp@gmail.com>
2026-03-02 00:42:38 -0300

    Config: @ActiveProfiles(test) in AbstractIntegrationTest.

--- supermarket-api/src/test/java/com/supermarket/integration/AbstractIntegrationTest.java
+++ supermarket-api/src/test/java/com/supermarket/integration/AbstractIntegrationTest.java
@@ -2,12 +2,14 @@ package com.supermarket.integration;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest(classes = com.supermarket.supermarket_api.SupermarketApiApplication.class)
 public abstract class AbstractIntegrationTest {
 
