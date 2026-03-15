06eea9ed405bfef61a6a677fac5e54682a3dfc63
jpdjcp <jpdjcp@gmail.com>
2026-03-15 02:32:09 -0300

    Refactor: injected JwtService into AuthController.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/AuthController.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/AuthController.java
@@ -2,9 +2,13 @@ package com.supermarket.supermarket_api.controller;
 
 import com.supermarket.supermarket_api.dto.auth.AuthResponse;
 import com.supermarket.supermarket_api.dto.auth.LoginRequest;
+import com.supermarket.supermarket_api.security.jwt.JwtService;
+import com.supermarket.supermarket_api.security.model.CustomUserDetails;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
     public AuthResponse login(LoginRequest login) {
 
-        authManager.authenticate(
+        Authentication authentication = authManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         login.username(),
                         login.password()
         ));
 
-        return new AuthResponse("Authentication successful");
+        CustomUserDetails userDetails =
+                (CustomUserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+        return new AuthResponse(token);
     }
 }
