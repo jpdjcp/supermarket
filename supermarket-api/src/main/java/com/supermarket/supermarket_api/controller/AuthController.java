1d324097367e28f63f31873ab6c2411dea1dfb6f
jpdjcp <jpdjcp@gmail.com>
2026-03-16 02:56:52 -0300

    Implemented: AuthController.signup().

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/AuthController.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/controller/AuthController.java
@@ -2,8 +2,12 @@ package com.supermarket.supermarket_api.controller;
 
 import com.supermarket.supermarket_api.dto.auth.AuthResponse;
 import com.supermarket.supermarket_api.dto.auth.LoginRequest;
+import com.supermarket.supermarket_api.dto.auth.SignupRequest;
+import com.supermarket.supermarket_api.dto.user.UserResponse;
 import com.supermarket.supermarket_api.security.jwt.JwtService;
 import com.supermarket.supermarket_api.security.model.CustomUserDetails;
+import com.supermarket.supermarket_api.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,9 +24,10 @@ public class AuthController {
 
     private final AuthenticationManager authManager;
     private final JwtService jwtService;
+    private final UserService userService;
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest login) {
+    public AuthResponse login(@RequestBody @Valid LoginRequest login) {
         Authentication authentication = authManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         login.username(),
@@ -33,5 +38,10 @@ public class AuthController {
                 (CustomUserDetails) authentication.getPrincipal();
         String token = jwtService.generateToken(userDetails);
         return new AuthResponse(token);
+    }
+
+    @PostMapping("/signup")
+    public UserResponse signup(@RequestBody @Valid SignupRequest signup) {
+        return userService.createUser(signup);
     }
 }
