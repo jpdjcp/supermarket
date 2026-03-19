f8cfd8973acf299c4365d8c74075f9e167243c47
jpdjcp <jpdjcp@gmail.com>
2026-03-19 20:52:54 -0300

    Add: supercharged UserNotFoundException with constructor with String username argument.

--- supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/UserNotFoundException.java
+++ supermarket-api/src/main/java/com/supermarket/supermarket_api/exception/UserNotFoundException.java
@@ -1,7 +1,12 @@
 package com.supermarket.supermarket_api.exception;
 
 public class UserNotFoundException extends DomainException {
+
     public UserNotFoundException(Long userId) {
         super("User not found with ID: " + userId);
+    }
+
+    public UserNotFoundException(String username) {
+        super("User not found with username: " + username);
     }
 }
